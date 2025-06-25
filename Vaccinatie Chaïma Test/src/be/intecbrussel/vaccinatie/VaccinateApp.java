6b66652760bc792f60e20f179e8d80da83c87863
Shayma1080 <hajji.chaima@outlook.fr>
2025-06-25 12:07:55 +0200

    update

--- Vaccinatie Chaïma Test/src/be/intecbrussel/vaccinatie/VaccinateApp.java
+++ Vaccinatie Chaïma Test/src/be/intecbrussel/vaccinatie/VaccinateApp.java
@@ -13,17 +13,17 @@ public class VaccinateApp {
         shelter.addAnimal(new Dog("Bello", 6));
         shelter.addAnimal(new Monkey("George", 10));
 
-        //shelter.vaccinateAnimal(Disease.CHICKENPOCKS);
-        System.out.println("Behandelt alle dieren: ");
-        shelter.treatAllAnimals(); // behandelt alle dieren
-        System.out.println("Sorteer dieren op leeftijd: ");
-        shelter.sortAnimalsByAge(); // sorteert op leeftijd
-        System.out.println(" Aantal dieren niet gevaccineerd tegen ziekte: ");
-        shelter.printAnimalsNotVaccinated(Disease.CHICKENPOCKS);// toont nietgevaccineerde dieren
-        System.out.println("Welke dier wordt behandelt? ");
-        shelter.treatAnimal("Bello");// behandelt specifiek dier op naam
 
-        Animal oudste = shelter.findOldestAnimal(); // zoekt oudste dier
+        System.out.println("Behandelt alle dieren: ");
+        shelter.treatAllAnimals();
+        System.out.println("\nSorteer dieren op leeftijd: ");
+        shelter.sortAnimalsByAge();
+        System.out.println("\nAantal dieren niet gevaccineerd tegen ziekte: ");
+        shelter.printAnimalsNotVaccinated(Disease.CHICKENPOCKS);
+        System.out.println("\nWelke dier wordt behandelt? ");
+        shelter.treatAnimal("Bello");
+
+        Animal oudste = shelter.findOldestAnimal();
         System.out.println("Oudste dier: " + oudste);
         System.out.println("Aantal dieren in het asiel: " + shelter.countAnimals());
 
